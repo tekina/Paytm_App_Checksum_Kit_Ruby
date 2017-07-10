e35d9f34c127876ace47f4fb625b6162170000a5
Aniket Garg <aniketgarg@outlook.com>
2017-07-10 15:06:32 +0530

    pass keys as args

--- generate_checksum.rb
+++ generate_checksum.rb
@@ -4,6 +4,6 @@ require './paytm/checksum_tool.rb'
 require 'uri'
 
 paytmHASH = { "MID" => ARGV[0], "ORDER_ID" => ARGV[1] }
-checksum_hash = ChecksumTool.new.get_checksum_hash(paytmHASH).gsub("\n",'')
+checksum_hash = ChecksumTool.new.get_checksum_hash(paytmHASH, ARGV[2]).gsub("\n",'')
 paytmHASH["CHECKSUMHASH"] =  checksum_hash
 puts "#{paytmHASH}"
--- paytm/checksum_tool.rb
+++ paytm/checksum_tool.rb
@@ -1,20 +1,19 @@
 class ChecksumTool
   include EncryptionNewPG
   
-  PAYTM_MERCHANT_KEY = ""
 
   def self.is_valid_merchant_id?(mid)
     return mid.eql?(MID)
   end
 
-  def get_checksum_hash(paytmparams)
-    return new_pg_checksum(paytmparams, PAYTM_MERCHANT_KEY)
+  def get_checksum_hash(paytmparams, key)
+    new_pg_checksum(paytmparams, key)
   end
-  def get_checksum_verified_array(paytmparams)
+
+  def get_checksum_verified_array(paytmparams, key)
     checksum_hash = paytmparams["CHECKSUMHASH"]
-		paytmparams.delete("CHECKSUMHASH")
-    return_array = paytmparams		
-		
-    return new_pg_verify_checksum(paytmparams, checksum_hash, PAYTM_MERCHANT_KEY)   
+    paytmparams.delete("CHECKSUMHASH")
+    return_array = paytmparams
+    return new_pg_verify_checksum(paytmparams, checksum_hash, key)
   end
 end
